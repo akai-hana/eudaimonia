797e6eb0185f0b120c77b0107712182f3a41cea4
akai-hana <akaihanatsundeee@proton.me>
2025-12-25 02:01:38 +0100

    add suckless; convenience auto-sync script

---
+++ auto-push.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# pushes changes to all repos inside the dir
+for dir in */; do
+    cd "$dir" || continue
+    
+    if [ -d .git ] || [ -f .git ]; then
+        echo "Syncing $dir"
+        git add .
+        git commit -m "automated sync"
+        git push
+    fi
+    
+    cd ..
+done
